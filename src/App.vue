2ee4e60d801680aa4865f7b1c275cd82fe5084c2
snzhr <snzhrski@gmail.com>
2021-12-02 15:47:04 +0600

    Added pagination

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
 <div class="content">
 <the-genres class="mt-10"></the-genres>
 <the-movie-list  :movielist="movies"></the-movie-list>
+  <the-pagination @get-movies="getNewMovies" :total-pages="totalPages" :total="total" :per-page="perPage" :current-page="currentPage"
+      :has-more-pages="hasMorePages" @pagechanged="showMore"></the-pagination>
 </div>
 </div>
 </template>
@@ -12,14 +14,23 @@
 import TheMovieList from '@/components/TheMovieList'
 import TheGenres from '@/components/TheGenres'
 import TheNavbar from '@/components/TheNavbar'
+import ThePagination from './components/ThePagination.vue'
+
 import api from './tmdbapi.js'
 export default {
   components:{
-    TheMovieList, TheGenres, TheNavbar
+    TheMovieList, TheGenres, TheNavbar, ThePagination
   },
   data(){
     return {
-        movies: []
+        movies: [],
+        page: 1,
+        totalPages: 0,
+        total: 0,
+        perPage: 20,
+        currentPage: 1,
+        hasMorePages: true
+
     }
   },
   mounted(){
@@ -27,8 +38,26 @@ export default {
     .then(response => response.json())
     .then(data =>{
       this.movies = data.results;
-      // console.log(data.results)
+      this.totalPages = data.total_pages;
+      this.page = data.page;
+      this.currentPage = data.page;
+      this.total = data.total_results;
+      console.log(data)
     })
+  },
+
+  methods:{
+    async getNewMovies(pageNum){
+      const res = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${api}&language=ru-Ru&page=${pageNum}`)
+      const data = await res.json();
+      this.movies = data.results;
+      // console.log(data);
+
+    },
+    showMore(page) {
+      this.page = page;
+      this.currentPage = page;
+    }
   }
 
 }
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="navigation flex justify-between items-center px-4 py-3 fixed z-10 w-full bg-green-700">
-      <div class="text-lg text-white">Vue Movie <i class="fad fa-laugh-wink"></i>
+  <div class="navigation flex justify-between items-center px-4 py-3 fixed z-10 w-full bg-green-600">
+      <div class="text-lg text-white">Vue3 Movie <i class="fad fa-laugh-wink"></i>
 </div>
       <div class="relative flex items-center">
           <input type="text" placeholder="Search movie..." class="rounded p-2 max-w-2xl focus:outline-none focus:ring focus:border-green-300">
-          <fa icon="search" class="absolute right-2 text-green-700 cursor-pointer "/>
+          <fa icon="search" class="absolute right-2 text-green-600 cursor-pointer bg-white"/>
       </div>
   </div>
 </template>

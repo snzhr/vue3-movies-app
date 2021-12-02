087f124f0a61516fc11793a95ef5e6ef093c104a
snzhr <snzhrski@gmail.com>
2021-12-03 00:22:22 +0600

    trailer fetch functin added

--- src/views/MoviePage.vue
+++ src/views/MoviePage.vue
@@ -3,13 +3,14 @@
 <div v-if="isMovieReady" class="flex justify-center items-center h-screen">
   <the-loader></the-loader>
 </div>
-<div v-else class="lg:flex px-4 pt-24 bg-gray-900">
+<div v-else class="lg:flex px-4 pt-24 bg-gray-900 lg:h-screen">
     <div class="mb-4 mr-4 lg:w-2/4">
   <img class="rounded" :src="preImg" :alt="movie.title">
     </div>
     <div class="lg:w-2/4 ml-4 text-white">
       <h1 class="text-3xl">{{movie.title}}</h1>
     <p>{{movie.overview}}</p>
+    <button @click="getTrailer" class="bg-gray-500 p-2 rounded mt-4 flex items-center">Смотреть трейлер <fa icon="play-circle" class="ml-1"/></button>
     </div>    
 </div>  
 </template>
@@ -26,7 +27,7 @@ export default {
   data(){
     return{
          movie: {},
-         preImg: ''
+         preImg: '',
     }
   },
   computed:{
@@ -39,9 +40,15 @@ export default {
     .then(res=>res.json())
     .then(data=>{
       this.movie = data
-      this.preImg = 'https://image.tmdb.org/t/p/w1280'+data.backdrop_path
+    this.preImg = 'https://image.tmdb.org/t/p/w1280'+data.backdrop_path
     })
-
+ },
+ methods:{
+   async getTrailer(){
+     const res = await fetch(`https://api.themoviedb.org/3/movie/${this.$route.params.id}/videos?api_key=${api}&language=ru-RU`)
+      const data = await res.json();
+      console.log(data);
+   }
  }  
 
 }

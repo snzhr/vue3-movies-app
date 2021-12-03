13757b0040bd42fb443a7e681a7d716fb63a1d09
snzhr <snzhrski@gmail.com>
2021-12-03 23:23:23 +0600

    loader added to search page

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,6 +1,10 @@
 <template>
      <div>
          <the-navbar></the-navbar>
+         <div v-if="loaded" class="flex justify-center items-center h-screen">
+                <the-loader></the-loader>
+            </div>
+         
          <div class="content mt-16 p-4">
          <the-movie-list  :movielist="foundMovies"></the-movie-list>
          </div>
@@ -11,15 +15,21 @@
 import api from '@/tmdbapi.js'
 import TheMovieList from '@/components/TheMovieList'
 import TheNavbar from '@/components/TheNavbar'
+import TheLoader from '@/components/UI/TheLoader.vue'
 
 export default {
     components:{
-        TheMovieList, TheNavbar
+        TheMovieList, TheNavbar, TheLoader
     },
-data(){
+    data(){
         return {
             foundMovies: []
         }
+    },
+    computed:{
+            loaded(){
+                return this.foundMovies.length === 0
+            }
     },
     async mounted(){
          const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api}&language=en-EN&query=${this.$route.params.name}`)
